9aa2810a005eae779cf72f7d99ecc7a947c5469f
mrsaurabhmaske <saurabh.r.maske@gmail.com>
2023-10-30 15:29:41 +0530

    Added extra Routes

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/users", (req, res) => {
+  res.send("Hello Users!");
+});
+
+app.get("/posts", (req, res) => {
+  res.send("Hello Posts!");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
